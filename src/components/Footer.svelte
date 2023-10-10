b03296b617c0ba9924ca2a9873c0cf92993bad4e
thehatclub <tanner.smiley321@gmail.com>
2023-10-09 19:13:55 -0500

    first

---
+++ src/components/Footer.svelte
@@ -0,0 +1,7 @@
+<footer>
+  Created by <a href="https://github.com/thehatclub" target="_blank"
+    >Tanner Smiley</a
+  >
+  using the MIT license.<br />Github repo can be
+  <a href="https://github.com/thehatclub/blog" target="_blank">found here</a>.
+</footer>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<header>
+  <h1>
+    <div class="icon baseline"><img src="/logo.png" alt="Logo" /></div>
+    Tanner's Blog
+  </h1>
+
+  <p>
+    A place where I explore the world of Computer Science as a college student
+  </p>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/projects">Projects</a>
+    <a href="/about">About</a>
+  </nav>
+</header>
